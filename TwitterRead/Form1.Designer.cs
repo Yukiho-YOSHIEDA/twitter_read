d23d996d5c1258e11c272b37b7ff0635db1f3ad9
saeyuki <is0436er@ed.ritsumei.ac.jp>
2018-08-16 00:54:40 +0900

    first commit

---
+++ TwitterRead/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TwitterRead
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.btn_disconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Location = new System.Drawing.Point(73, 12);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(133, 59);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "CONNECT";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(73, 95);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(133, 59);
+            this.btn_send.TabIndex = 0;
+            this.btn_send.Text = "SEND";
+            this.btn_send.UseVisualStyleBackColor = true;
+            // 
+            // btn_disconnect
+            // 
+            this.btn_disconnect.Location = new System.Drawing.Point(73, 172);
+            this.btn_disconnect.Name = "btn_disconnect";
+            this.btn_disconnect.Size = new System.Drawing.Size(133, 59);
+            this.btn_disconnect.TabIndex = 0;
+            this.btn_disconnect.Text = "DISCONNECT";
+            this.btn_disconnect.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_disconnect);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Button btn_disconnect;
+    }
+}
+
---
+++ TwitterRead/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TwitterRead
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient tc = null;
+        private NetworkStream ns = null;
+        private BinaryWriter bw = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btn_connect.Click += connect;
+            btn_send.Click += playTest;
+            btn_disconnect.Click += disconnect;
+
+            btn_send.Enabled = false;
+            btn_disconnect.Enabled = false;
+        }
+
+        private void connect(object sender, EventArgs e)
+        {
+            //棒読みちゃんのTCPサーバへ接続
+            string sHost = "127.0.0.1"; //棒読みちゃんが動いているホスト
+            int iPort = 50001;       //棒読みちゃんのTCPサーバのポート番号(デフォルト値)
+            try
+            {
+                tc = new TcpClient(sHost, iPort);
+                ns = tc.GetStream();
+                bw = new BinaryWriter(ns);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("接続失敗");
+            }
+
+            btn_connect.Enabled = false;
+            btn_send.Enabled = true;
+            btn_disconnect.Enabled = true;
+        }
+
+        private void disconnect(object sender, EventArgs e)
+        {
+            if(tc != null)
+            {
+                //切断
+                tc.Close();
+
+                tc = null;
+
+                btn_connect.Enabled = true;
+                btn_send.Enabled = false;
+                btn_disconnect.Enabled = false;
+            }
+        }
+
+        private void playTest(object sender, EventArgs e)
+        {
+            string sMessage = "テストだよ";
+            byte bCode = 0;
+            Int16 iVoice = 1;
+            Int16 iVolume = -1;
+            Int16 iSpeed = -1;
+            Int16 iTone = -1;
+            Int16 iCommand = 0x0001;
+
+            byte[] bMessage = Encoding.UTF8.GetBytes(sMessage);
+            Int32 iLength = bMessage.Length;
+
+            if (tc != null)
+            {
+                //メッセージ送信
+                try
+                {
+                    bw.Write(iCommand); //コマンド（ 0:メッセージ読み上げ）
+                    bw.Write(iSpeed);   //速度    （-1:棒読みちゃん画面上の設定）
+                    bw.Write(iTone);    //音程    （-1:棒読みちゃん画面上の設定）
+                    bw.Write(iVolume);  //音量    （-1:棒読みちゃん画面上の設定）
+                    bw.Write(iVoice);   //声質    （ 0:棒読みちゃん画面上の設定、1:女性1、2:女性2、3:男性1、4:男性2、5:中性、6:ロボット、7:機械1、8:機械2、10001～:SAPI5）
+                    bw.Write(bCode);    //文字列のbyte配列の文字コード(0:UTF-8, 1:Unicode, 2:Shift-JIS)
+                    bw.Write(iLength);  //文字列のbyte配列の長さ
+                    bw.Write(bMessage); //文字列のbyte配列
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+    }
+}
